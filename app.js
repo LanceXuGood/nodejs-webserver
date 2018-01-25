9ec8e71782477a0ec52dd8733fcfdbe41071c4eb
Lance <LanceXuGood@gmail.com>
2018-01-26 02:43:00 +0800

    feat: 初始化

---
+++ app.js
@@ -0,0 +1,52 @@
+
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const staticServer = require('koa-static');
+const fs = require('fs');
+const request = require('superagent');
+
+
+const app = new Koa();
+const router = new Router();
+var path = require('path');
+
+app.use(staticServer(path.join(__dirname, './public')));
+
+
+router.get('/', async   (ctx, next) => {
+    ctx.body = '123';
+});
+// 还要优化很久哈哈，慢慢做把
+router.get('/api/*', async(ctx, next) => {
+   
+
+    const method = ctx.request.method.toLowerCase();
+    const originalUrl = ctx.request.originalUrl;
+    // if (method === 'post' || method === 'put') {
+    //     sreq.set('Content-Type', 'application/x-www-form-urlencoded')
+    //         .send(ctx.request.body);
+    //     const data = await request[method]('https://api.douban.com' + originalUrl);
+    // }
+
+    // 数据签名
+    // https://api-m.mtime.cn/Showtime/HotCitiesByCinema.api
+    //如果为 post 或者 put 则需要发送时传递body
+    const url = ('https://api-m.mtime.cn' + originalUrl).replace(/\/api\//,'/')
+    console.log(method)
+    if (method === 'post' || method === 'put') {
+        const data  = await request[method](url).set('Content-Type', 'application/json');
+        ctx.body = data.body;
+
+    }else{
+        const data  = await request[method](url);
+        console.log(data)
+        ctx.body = data.body;
+    }
+
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(3000);
