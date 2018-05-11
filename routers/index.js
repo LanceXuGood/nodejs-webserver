0a98111be0a61611adf1a3bd1dc387d61eddd859
lance <lancexugood@gmail.com>
2018-05-11 13:13:40 +0800

    js 安全域名验证

--- routers/index.js
+++ routers/index.js
@@ -6,12 +6,13 @@ const {
   sqlData
 } = require('../until/index');
 // 测试api接口
-// router.get('/', async (ctx, next) => {
-//   ctx.body = {
-//     data: "成功",
-//     status: 200
-//   };
-// });
+router.get('/', async (ctx, next) => {
+  ctx.body = {
+    data: "成功",
+    status: 200
+  };
+});
+
 
 
 // 豆瓣接口
