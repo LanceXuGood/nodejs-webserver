8c5d1a30080f57c74c6ef085e39ca62ee4e9ca23
lance <lancexugood@gmail.com>
2018-05-11 15:01:43 +0800

    测试分享-1

--- public/share.js
+++ public/share.js
@@ -17,7 +17,7 @@ axios.post('http://www.webcodelance.cn/wx/jsSdk', {
     wx.ready(function () {
         wx.onMenuShareTimeline({
             title: '123', // 分享标题
-            link: 'http://wx.webcodelance.cn/123', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: 'http://img0.imgtn.bdimg.com/it/u=1999380000,1378130272&fm=214&gp=0.jpg', // 分享图标
             success: function () {
                 console.log('ok');
@@ -28,7 +28,7 @@ axios.post('http://www.webcodelance.cn/wx/jsSdk', {
         });
         wx.onMenuShareAppMessage({
             title: '123', // 分享标题
-            link: 'http://wx.webcodelance.cn/123', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: 'http://img0.imgtn.bdimg.com/it/u=1999380000,1378130272&fm=214&gp=0.jpg', // 分享图标
             success: function () {
                 console.log('ok');
